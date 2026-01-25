b75290842f31138367ee470096ae364016fab5d4
taylin damonze <82763280+taylin1@users.noreply.github.com>
2026-01-25 21:38:03 +0200

    Created maintenance request

--- backend/routes/maintenance.js
+++ backend/routes/maintenance.js
@@ -4,4 +4,9 @@ const router = express.Router();
 const supabase = require("../supabaseClient");
 const auth = require("../middleware/auth");
 
-
+router.post("/",auth, async (req,res) =>{
+    const{description, propety_id} = req.body;
+    const tenant_id = req.user.id;
+    
+}
+)
