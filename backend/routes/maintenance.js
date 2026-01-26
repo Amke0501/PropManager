04fd6f64ae0092b8c7a2ce66502ac0958ae1ad1f
taylin damonze <82763280+taylin1@users.noreply.github.com>
2026-01-26 11:17:45 +0200

    Added route requests to maintenance file

--- backend/routes/maintenance.js
+++ backend/routes/maintenance.js
@@ -3,10 +3,69 @@ const express = require("express");
 const router = express.Router();
 const supabase = require("../supabaseClient");
 const auth = require("../middleware/auth");
+const requireAdmin = require("../middleware/rbac");
 
-router.post("/",auth, async (req,res) =>{
-    const{description, propety_id} = req.body;
-    const tenant_id = req.user.id;
-    
-}
-)
+
+//Creates a maintenance request
+router.post("/", auth, async (req, res) => {
+  const { title, description, property_id } = req.body;
+  const tenant_id = req.user.id;
+
+  //Chooses table and data to it
+  const { data, error } = await supabase
+  .from("maintenance_requests")
+  .insert([
+    {
+      title,
+      description,
+      property_id,
+      tenant_id,
+      status: "pending",
+    },
+  ]);
+
+  // If superbase fails sends error else it is created
+  if (error) {
+    return res.status(400).json({ error: error.message });
+  }
+
+  res.status(201).json({
+     message: "Maintenance request created", data 
+    });
+});
+
+//User must be logged in then check role then sends database query 
+router.get("/", auth, async (req, res) => {
+  const role = req.user.role;
+  let query = supabase.from("maintenance_requests").select("*");
+
+    if (role === "tenant") {
+    query = query.eq("tenant_id", req.user.id);
+  }
+
+   const { data, error } = await query;
+
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json(data);
+});
+
+  //Updates maintenace status , only admins allowed
+  router.put("/:id", auth, requireAdmin, async (req, res) => {
+  const { status } = req.body;
+  const { id } = req.params;
+
+  const { data, error } = await supabase
+    .from("maintenance_requests")
+    .update({ status })
+    .eq("id", id);  // Update only one row
+
+      if (error) return res.status(400).json({ error: error.message });
+
+  res.json({ message: "Status updated", data });
+});
+
+  
+
+
+
