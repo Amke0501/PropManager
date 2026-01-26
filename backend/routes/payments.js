28c76270a71d3bc482188eb2aebf6ccd225c03fa
taylin damonze <82763280+taylin1@users.noreply.github.com>
2026-01-26 13:19:27 +0200

    Added routing for payment file

---
+++ backend/routes/payments.js
@@ -0,0 +1,29 @@
+
+
+router.post("/", auth, requireAdmin, async (req, res) => {
+  const { tenant_id, amount, month } = req.body;
+
+   const { data, error } = await supabase
+    .from("payments")
+    .insert([{ tenant_id, amount, month }]);
+
+   if (error) return res.status(400).json({ error: error.message });
+
+  res.status(201).json({ message: "Payment recorded", data });
+});
+
+router.get("/", auth, async (req, res) => {
+  const role = req.user.role;
+  let query = supabase.from("payments").select("*");
+
+    if (role === "tenant") {
+    query = query.eq("tenant_id", req.user.id);
+  }
+
+     const { data, error } = await query;
+
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json(data);
+});
+
