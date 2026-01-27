da3bccf830037a123a3521b16f96ef90def8eebb
Tholakele-jiyana <tholakelejiyana@gmail.com>
2026-01-28 01:35:02 +0200

    Created the skeleton hero section

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -4,9 +4,18 @@ import {Header} from "../components/Header/Header.jsx";
 
 export const Home = () => {
     return (
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen bg-gray-50">  {/*Haven't decided on the colour scheme yet*/}
         <Header />
         {/*<DashboardAdmin/>*/}
+
+        {/*HERO SECTION*/}
+        <section>
+            <div>
+                <h1>Prop.Manger</h1>
+                <p>We manage properties, liase with tenants and attend to all related maintenance request so that that you can focus on what matters most.</p>
+            </div>
+        </section>
+
         </div>
     )
 }
