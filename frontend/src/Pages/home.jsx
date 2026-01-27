9d70ab5b1820174fe24f84f962e82b09cf0c398c
Tholakele-jiyana <tholakelejiyana@gmail.com>
2026-01-28 01:11:11 +0200

    styled the backgound colour

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -4,7 +4,7 @@ import {Header} from "../components/Header/Header.jsx";
 
 export const Home = () => {
     return (
-        <div>
+        <div className="min-h-screen bg-gray-50">
         <Header />
         {/*<DashboardAdmin/>*/}
         </div>
