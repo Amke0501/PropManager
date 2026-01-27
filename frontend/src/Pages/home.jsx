0fd4717ced97801ee62e623f58a25b2b7a5335d4
Tholakele-jiyana <tholakelejiyana@gmail.com>
2026-01-28 01:06:32 +0200

    Commented out the DashboardAdmin on the landing page

--- frontend/src/Pages/home.jsx
+++ frontend/src/Pages/home.jsx
@@ -1,11 +1,12 @@
 import {Header} from "../components/Header/Header.jsx";
-import {DashboardAdmin} from "../components/dashboard-admin/dashboard-admin.jsx";
+//import {DashboardAdmin} from "../components/dashboard-admin/dashboard-admin.jsx";
+// should DashboardAdmin be in the landing page?
 
 export const Home = () => {
     return (
         <div>
         <Header />
-        <DashboardAdmin/>
+        {/*<DashboardAdmin/>*/}
         </div>
     )
 }
