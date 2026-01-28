27c645c043bb2beb3cae46f8122c7f9c12e3021c
taylin damonze <82763280+taylin1@users.noreply.github.com>
2026-01-28 11:00:44 +0200

    Added updated auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,36 @@
+const jwt = require("jsonwebtoken");
+
+const requireAuth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  // Check if Authorization header exists
+  if (!authHeader) {
+    return res.status(401).json({ error: "Not authenticated" });
+  }
+
+  //  Bearer TOKEN
+  const token = authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Not authenticated" });
+  }
+
+  try {
+    //Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    // Attach user info to request
+    req.user = {
+      id: decoded.id,
+      role: decoded.role,
+      email: decoded.email
+    };
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
+
+module.exports = requireAuth;
 
