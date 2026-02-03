9760a0e8db0a288557bb9f7c500b4fcc5afb7bdd
Tholakele-jiyana <tholakelejiyana@gmail.com>
2026-02-03 13:49:13 +0200

    Restored to original admin code, created home page and rerouted home/landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {Home} from "./Pages/home.jsx"; 
 import {Adminhome} from "./Pages/adminhome.jsx";
 import {NotFound} from "./Pages/notFound.jsx";
 import {UserHome} from "./Pages/userhome.jsx";
@@ -11,6 +12,7 @@ function App() {
        <BrowserRouter>
            <Routes>
                <Route index element={<Adminhome />} />
+               <Route path = "/landing" element={<Home/>} />
                <Route index path="/user" element={<UserHome />} />
                <Route path="*" element={<NotFound />} />
            </Routes>
