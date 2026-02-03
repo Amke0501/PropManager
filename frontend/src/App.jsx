b560bb3a7093597fe80c09612219fb7214ac625d
LesMrCode <mhonelesleyeric@gmail.com>
2026-02-03 18:51:14 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Home} from "./Pages/home.jsx";
 import {NotFound} from "./Pages/notFound.jsx";
+import "./components/Login.jsx"
 
 function App() {
   return (
     <>
-   
+   <Login />
        <BrowserRouter>
            <Routes>
                <Route index element={<Home />} />
